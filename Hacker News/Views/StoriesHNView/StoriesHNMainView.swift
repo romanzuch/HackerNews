b9c1499bdd8dd50bbd06dfdb0e72f9d80c6fc4e6
Roman <roman.zuchowski@gmail.com>
2021-09-19 01:45:55 +0200

    initial commit storiesHNMainView

--- Hacker News/Views/StoriesHNView/StoriesHNMainView.swift
+++ Hacker News/Views/StoriesHNView/StoriesHNMainView.swift
@@ -10,70 +10,36 @@ import SwiftUI
 struct StoriesHNMainView: View {
     
     @EnvironmentObject var networkManager: HNNetworkManager
-    @State private var selectedCategory: StoryCategories = .new
+    @State private var selectedCategory: StoryCategory = .new
     
     var body: some View {
         
         NavigationView {
-            VStack {
-                
-                switch selectedCategory {
-                
-                case .new:
-                    NewStoriesHNView()
-                        .environmentObject(networkManager)
-                case .best:
-                    BestStoriesHNView()
-                        .environmentObject(networkManager)
-                case .top:
-                    TopStoriesHNView()
-                        .environmentObject(networkManager)
-                    
-                }
-                
-                Spacer()
+            switch selectedCategory {
+            
+            case .new:
+                NewStoriesHNView()
+                    .environmentObject(networkManager)
+                    .navigationBarTitle("Stories")
+                    .navigationBarItems(trailing:
+                        MenuButton($selectedCategory)
+                    )
+            case .best:
+                BestStoriesHNView()
+                    .environmentObject(networkManager)
+                    .navigationBarTitle("Stories")
+                    .navigationBarItems(trailing:
+                        MenuButton($selectedCategory)
+                    )
+            case .top:
+                TopStoriesHNView()
+                    .environmentObject(networkManager)
+                    .navigationBarTitle("Stories")
+                    .navigationBarItems(trailing:
+                        MenuButton($selectedCategory)
+                    )
                 
             }
-            .navigationBarTitle("Stories")
-            .navigationBarItems(trailing:
-                Menu {
-                    
-                    Button(action: {
-                        selectedCategory = .new
-                    }, label: {
-                        if selectedCategory == .new {
-                            Label("New", systemImage: "checkmark")
-                        } else {
-                            Text("New")
-                        }
-                    })
-                    
-                    Button(action: {
-                        selectedCategory = .best
-                    }, label: {
-                        if selectedCategory == .best {
-                            Label("Best", systemImage: "checkmark")
-                        } else {
-                            Text("Best")
-                        }
-                    })
-                    
-                    Button(action: {
-                        selectedCategory = .top
-                    }, label: {
-                        if selectedCategory == .top {
-                            Label("Top", systemImage: "checkmark")
-                        } else {
-                            Text("Top")
-                        }
-                    })
-                    
-                } label: {
-                    
-                    Image(systemName: "switch.2")
-                    
-                }
-            )
         }
     }
 }
