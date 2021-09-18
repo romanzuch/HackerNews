ebe5fc02cdbf0d40a0fd0a38b9f66d59b7bdd8b0
Roman <roman.zuchowski@gmail.com>
2021-09-18 19:09:05 +0200

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Roman on 18.09.21.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
