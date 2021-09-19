fdeb602dd39414533494bc7dd771f67f7fb120fc
Roman <roman.zuchowski@gmail.com>
2021-09-19 12:36:16 +0200

    separators, time since post, refined UI

--- Hacker News/Views/ListElements/ListElementView.swift
+++ Hacker News/Views/ListElements/ListElementView.swift
@@ -12,13 +12,15 @@ struct ListElementView: View {
     var story: Story
     let dateFormatter = DateFormatter()
     var timeString: String
+    let formatter = RelativeDateTimeFormatter()
     
     init(_ story: Story) {
         self.story = story
         self.dateFormatter.timeStyle = DateFormatter.Style.short
         self.dateFormatter.dateStyle = DateFormatter.Style.short
         self.dateFormatter.timeZone = .current
-        self.timeString = self.dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(story.time)))
+        formatter.unitsStyle = .short
+        self.timeString = formatter.localizedString(for: Date(timeIntervalSince1970: TimeInterval(story.time)), relativeTo: Date())
     }
     
     var body: some View {
@@ -26,8 +28,13 @@ struct ListElementView: View {
             Text(story.title).font(.system(size: 14.0)).lineLimit(1)
             HStack {
                 Text(story.user).font(.system(size: 10.0)).fontWeight(.bold)
-                Text("\(story.comments.count) comment(s)").font(.system(size: 10.0))
+                Text("\u{00B7}")
                 Text("\(self.timeString)").font(.system(size: 10.0))
+            }
+            HStack {
+                Text(story.score != 1 ? "\(story.score) points" : "\(story.score) point").font(.system(size: 10.0))
+                Text("\u{00B7}")
+                Text(story.comments.count != 1 ? "\(story.comments.count) comments" : "\(story.comments.count) comment").font(.system(size: 10.0))
             }
         }
     }
