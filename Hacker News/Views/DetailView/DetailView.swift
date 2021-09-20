7930bdc8d43e59f115aa42dcb792e2c47b598f05
Roman <roman.zuchowski@gmail.com>
2021-09-20 20:30:35 +0200

    changed networkManager instance to env-object, made the comment text conditional, as well as the list of comments

--- Hacker News/Views/DetailView/DetailView.swift
+++ Hacker News/Views/DetailView/DetailView.swift
@@ -14,7 +14,7 @@ struct DetailView: View {
     let dateFormatter = DateFormatter()
     var timeString: String
     let formatter = RelativeDateTimeFormatter()
-    let networkManager: HNNetworkManager = HNNetworkManager()
+    @EnvironmentObject var networkManager: HNNetworkManager
     
     init(_ story: Story) {
         self.story = story
@@ -32,19 +32,28 @@ struct DetailView: View {
             HStack {
                 Text(story.user).font(.system(size: 10.0)).fontWeight(.bold)
                 Text("\u{00B7}")
-//                Text(story.comments.count != 1 ? "\(story.comments.count) comments" : "\(story.comments.count) comment").font(.system(size: 10.0))
+                Group {
+                    if (story.comments != nil) {
+                        Text(story.comments!.count != 1 ? "\(story.comments!.count) comments" : "\(story.comments!.count) comment").font(.system(size: 10.0))
+                    } else {
+                        Text("0 comments").font(.system(size: 10.0))
+                    }
+                }
                 Text("\u{00B7}")
                 Text("\(self.timeString)").font(.system(size: 10.0))
             }
             
             // Title
             Text(story.title)
+                .padding(.horizontal, 10)
+                .padding(.vertical, 10)
             
             // URL
             Group {
                 if (story.url != nil) {
                     Link(destination: URL(string: story.url!)!, label: {
                         Text(story.url!).font(.system(size: 10.0))
+                            .padding(.horizontal, 10)
                     })
                 } else {
                     EmptyView()
@@ -52,11 +61,36 @@ struct DetailView: View {
             }
             
             // Comments
-//            Group {
-//                ForEach(story.comments, id: \.self) { comment in
-//                    Text("\(networkManager.requestComment(comment).text)")
-//                }
-//            }
+            Group {
+                if (story.comments != nil) {
+                    ScrollView {
+                        ForEach(story.comments!, id: \.self) { id in
+                            Group {
+                                if networkManager.storyComments.keys.contains(id) {
+                                    Text("\(networkManager.storyComments[id]!.text)")
+                                        .font(.system(size: 10.0))
+                                        .padding(.all, 10)
+                                        .unredacted()
+                                } else {
+                                    Text(networkManager.storyComments.keys.contains(id) ? "\(networkManager.storyComments[id]!.text)" : "This could be a comment...")
+                                        .font(.system(size: 10.0))
+                                        .padding(.all, 10)
+                                        .redacted(reason: .placeholder)
+                                }
+                            }
+                        }
+                    }
+                    
+                    .onAppear {
+                        networkManager.requestComments(story.comments!)
+                    }
+                    
+                } else {
+                    Text("No comments so far...")
+                }
+            }
+            
+            
             Spacer()
         }
         .navigationBarItems(trailing:
