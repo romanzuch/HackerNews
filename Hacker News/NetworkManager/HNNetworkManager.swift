1c0db7cdd24a109c78d575a8fbc7f3ee41e3e19d
Roman <roman.zuchowski@gmail.com>
2021-09-20 20:31:02 +0200

    added new published variable to store a story's comments, new comment request method

--- Hacker News/NetworkManager/HNNetworkManager.swift
+++ Hacker News/NetworkManager/HNNetworkManager.swift
@@ -19,6 +19,9 @@ class HNNetworkManager: ObservableObject {
     @Published var newStories: [Int:Story] = [:]
     @Published var bestStories: [Int:Story] = [:]
     
+    // Story Comments
+    @Published var storyComments: [Int:Comment] = [:]
+    
     func request(_ url: apiURL) {
         
         guard let urlRawValue = URL(string: url.rawValue) else { return }
@@ -29,8 +32,7 @@ class HNNetworkManager: ObservableObject {
                 requestIDs(urlRawValue, apiURL: url) { result in
                     switch result {
                     case .success:
-                        print("success")
-//                        requestStories(topStoryIDs, cat: .top)
+                        print("top stories requested")
                     case .failure(let error):
                         print(error.localizedDescription)
                     }
@@ -44,8 +46,7 @@ class HNNetworkManager: ObservableObject {
                 requestIDs(urlRawValue, apiURL: url) { result in
                     switch result {
                     case .success:
-                        print("success")
-//                        requestStories(newStoryIDs, cat: .new)
+                        print("new stories requested")
                     case .failure(let error):
                         print(error.localizedDescription)
                     }
@@ -59,8 +60,7 @@ class HNNetworkManager: ObservableObject {
                 requestIDs(urlRawValue, apiURL: url) { result in
                     switch result {
                     case .success:
-                        print("success")
-//                        requestStories(bestStoryIDs, cat: .best)
+                        print("best stories requested")
                     case .failure(let error):
                         print(error.localizedDescription)
                     }
@@ -114,13 +114,6 @@ class HNNetworkManager: ObservableObject {
         
     }
     
-//    private func requestStories(_ IDs: Array<Int>, cat: StoryCategory) {
-//        print(cat.rawValue)
-//        for id in IDs {
-//            requestStory(id, cat: cat)
-//        }
-//    }
-    
     func requestStory(_ id: Int, cat: StoryCategory) {
         
         let url = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(id).json?print=pretty")!
@@ -133,7 +126,6 @@ class HNNetworkManager: ObservableObject {
                 URLSession.shared.dataTask(with: url) { (data, response, error) in
                     guard let data = data else { return }
                     let jsonDecoder = JSONDecoder()
-//                    print(String(data: data, encoding: .utf8))
                     if let story = try? jsonDecoder.decode(Story.self, from: data) {
                         DispatchQueue.main.async {
                             self.newStories[id] = story
@@ -149,7 +141,6 @@ class HNNetworkManager: ObservableObject {
                 URLSession.shared.dataTask(with: url) { (data, response, error) in
                     guard let data = data else { return }
                     let jsonDecoder = JSONDecoder()
-//                    print(String(data: data, encoding: .utf8))
                     if let story = try? jsonDecoder.decode(Story.self, from: data) {
                         DispatchQueue.main.async {
                             self.bestStories[id] = story
@@ -165,7 +156,6 @@ class HNNetworkManager: ObservableObject {
                 URLSession.shared.dataTask(with: url) { (data, response, error) in
                     guard let data = data else { return }
                     let jsonDecoder = JSONDecoder()
-//                    print(String(data: data, encoding: .utf8))
                     if let story = try? jsonDecoder.decode(Story.self, from: data) {
                         DispatchQueue.main.async {
                             self.topStories[id] = story
@@ -178,8 +168,35 @@ class HNNetworkManager: ObservableObject {
         
     }
     
-    func requestComment(_ id: Int) -> Comment {
-        return Comment(user: "stingraycharles", id: 28578541, comments: [28578595, 28578629], parent: 28578399, text: "Since this is HN, it’s 2021 and DDoS’es are still a thing:  why are they still a thing? Is there some fundamental “anonymity” to the Internet that makes it impossible to structurally prevent DDoS attacks? Apart from CloudFlare-like approaches, are there any R&amp;D in the pipeline that may kill this type of attack once and for all?<p>To me it’s incredibly infuriating to see the damage that still happens with these extremely simple techniques. Will it ever end?<p>Edit: to elaborate, I know that there are tons of insecure Internet devices and whatnot. I’m more interested in standards, and core protocol improvements that can fundamentally rid the world of these types of attacks.", time: 1631992891, type: "comment")
+    func requestComment(_ id: Int) {
+        
+        let url: URL = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(id).json?print=pretty")!
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
+            let jsonDecoder = JSONDecoder()
+            if let comment = try? jsonDecoder.decode(Comment.self, from: data) {
+                DispatchQueue.main.async {
+                    self.storyComments[id] = comment
+                }
+            }
+        }.resume()
+        
+    }
+    
+    func requestComments(_ ids: [Int]) {
+        for id in ids {
+            let url: URL = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(id).json?print=pretty")!
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                guard let data = data else { return }
+                let jsonDecoder = JSONDecoder()
+                if let comment = try? jsonDecoder.decode(Comment.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.storyComments[id] = comment
+                    }
+                }
+            }.resume()
+        }
     }
     
     
