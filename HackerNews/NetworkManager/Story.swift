e3a532da6b2ef01d0a88013eb7e06a1cf32a1778
Roman <roman.zuchowski@gmail.com>
2021-09-29 22:38:04 +0200

    HN-52 #comment story response is now decoded conditionally

--- HackerNews/NetworkManager/Story.swift
+++ HackerNews/NetworkManager/Story.swift
@@ -10,14 +10,36 @@ import Foundation
 struct Story: Codable, Hashable {
     
     var by: String
-    var descendants: Int
+    var descendants: Int?
     var id: Int
-    var kids: [Int]
+    var kids: [Int]?
     var score: Int
     var time: Int
     var title: String
     var type: String
-    var url: String
+    var url: String?
+    
+    enum CodingKeys: CodingKey {
+        
+        case by, descendants, id, kids, score, time, title, type, url
+        
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.by = try container.decode(String.self, forKey: .by)
+        self.id = try container.decode(Int.self, forKey: .id)
+        self.score = try container.decode(Int.self, forKey: .score)
+        self.time = try container.decode(Int.self, forKey: .time)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.type = try container.decode(String.self, forKey: .type)
+        
+        self.descendants = try container.decodeIfPresent(Int.self, forKey: .descendants)
+        self.url = try container.decodeIfPresent(String.self, forKey: .url)
+        
+    }
     
 }
 
