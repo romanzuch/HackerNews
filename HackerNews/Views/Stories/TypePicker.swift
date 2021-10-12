ef3bfc41a6cd411e9277b72396120d133e294bc2
Roman <roman.zuchowski@gmail.com>
2021-10-12 21:33:09 +0200

    HN-61 #comment added border to text view

--- HackerNews/Views/Stories/TypePicker.swift
+++ HackerNews/Views/Stories/TypePicker.swift
@@ -19,8 +19,14 @@ struct TypePicker: View {
         
         HStack {
             ForEach(RequestType.allCases, id: \.self) { type in
+                
                 Text("\(type.rawValue)")
                     .fontWeight(selection == type ? .bold : .none)
+                    .frame(width: UIScreen.main.bounds.width / 3 - 20)
+                    .frame(height: 40)
+                
+                    .border(width: (selection == type) ? 5 : 0, edges: [.bottom], color: .blue.opacity(0.25))
+                    
                     .onTapGesture {
                         selection = type
                     }
@@ -29,3 +35,9 @@ struct TypePicker: View {
         
     }
 }
+//
+//struct TypePicker_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TypePicker(.constant(.new))
+//    }
+//}
