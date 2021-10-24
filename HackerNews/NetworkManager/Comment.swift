76e7b8ede97fe99bbc8edddcf5ba5e3ae1bfd7dc
Roman <roman.zuchowski@gmail.com>
2021-10-24 14:36:40 +0200

    HN-72 #comment updated Comment type to map all different response options

--- HackerNews/NetworkManager/Comment.swift
+++ HackerNews/NetworkManager/Comment.swift
@@ -9,17 +9,22 @@ import Foundation
 
 struct Comment: Hashable, Codable {
     
-    var by: String
+    var by: String?
     var id: Int
     var kids: [Int]?
     var parent: Int
-    var text: String
+    var text: String?
     var time: Int
     var type: String
+    
+    // deleted comments have an additional property
+    // deleted, id, parent, time, type, ...
+    var deleted: Bool?
     
     enum CodingKeys: CodingKey {
         
         case by, id, kids, parent, text, time, type
+        case deleted
         
     }
     
@@ -27,14 +32,16 @@ struct Comment: Hashable, Codable {
         
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-        self.by = try container.decode(String.self, forKey: .by)
+        
         self.id = try container.decode(Int.self, forKey: .id)
         self.parent = try container.decode(Int.self, forKey: .parent)
         self.time = try container.decode(Int.self, forKey: .time)
-        self.text = try container.decode(String.self, forKey: .text)
         self.type = try container.decode(String.self, forKey: .type)
         
         self.kids = try container.decodeIfPresent([Int].self, forKey: .kids)
+        self.deleted = try container.decodeIfPresent(Bool.self, forKey: .deleted)
+        self.text = try container.decodeIfPresent(String.self, forKey: .text)
+        self.by = try container.decodeIfPresent(String.self, forKey: .by)
         
     }
     
