78805b8d8ade43a7c81ef5359d4da7cf94b3b432
Roman <roman.zuchowski@gmail.com>
2021-11-02 19:56:42 +0100

    HN-85 #comment Initial declaration of comment VM and pass it to the main view of the app

--- HackerNews/HackerNewsApp.swift
+++ HackerNews/HackerNewsApp.swift
@@ -12,12 +12,14 @@ struct HackerNewsApp: App {
     
     @StateObject var storyViewModel: StoryViewModel = StoryViewModel()
     @StateObject var settingsViewModel: SettingsViewModel = SettingsViewModel()
+    @StateObject var commentViewModel: CommentViewModel = CommentViewModel()
     
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(storyViewModel)
                 .environmentObject(settingsViewModel)
+                .environmentObject(commentViewModel)
                 .preferredColorScheme(settingsViewModel.getAppAppearance())
         }
     }
