9def1fc6822d27dacff60617d86e6e4b14338bf7
Roman <roman.zuchowski@gmail.com>
2021-11-02 19:25:36 +0100

    HN-79 #comment Added environment object to pass settings VM to the stories view

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
                 }
                 .tag(0)
                 .environmentObject(storyViewModel)
+                .environmentObject(settingsViewModel)
             
             SavedView()
                 .tabItem {
